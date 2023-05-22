0a16f0029ebe6bf2703e5e8aecd905546fd8c5b9
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-05-22 18:53:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+//   //write your code here
+//   document.querySelector(".myDomainName").innerHTML = domainNameGenerator()
+// };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+domainNameGenerator = () => {
+  let pronouns = ['the','our'];
+  let adjs = ['great', 'big' ];
+  let nouns = ['jogger','racoon'];
+  let tlds = ['.com', '.se', '.co.uk', '.us', '.fr']
+
+  domainArray = []
+  for (pronoun of pronouns){
+    for (adj of adjs){
+      for (noun of nouns){
+        for (tld of tlds){
+          console.log(pronoun+adj+noun+tld)  // if we rather print one by one in the console
+          // domainArray.push( (pronoun+adj+noun+tld).toString() ) // if we rather print an array with all domain name possibilites
+        }
+      }
+    }
+  }
+  return domainArray
+}
+
+domainNameGenerator()
+//console.log(domainNameGenerator())
